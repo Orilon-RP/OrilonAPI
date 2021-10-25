8a9f1b0ec3455a9e7c2257ef72a8ee74c1590199
Mouah1er <jallc2016>
2021-10-25 21:28:52 +0200

    Change updateUser parameter

--- src/main/java/fr/orilon/api/managers/UserManager.java
+++ src/main/java/fr/orilon/api/managers/UserManager.java
@@ -4,7 +4,6 @@ import fr.orilon.api.users.User;
 import org.bukkit.entity.Player;
 
 import java.util.UUID;
-import java.util.function.Consumer;
 
 public interface UserManager {
     void createUser(Player player);

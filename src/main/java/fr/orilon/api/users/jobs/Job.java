aa40ee8c8032b309eafbdc816f10bd8a6963479c
Mouah1er <jallc2016>
2021-12-18 13:58:17 +0100

    Change Jobs

--- src/main/java/fr/orilon/api/users/jobs/Job.java
+++ src/main/java/fr/orilon/api/users/jobs/Job.java
@@ -3,22 +3,10 @@ package fr.orilon.api.users.jobs;
 import fr.orilon.api.users.jobs.tasks.JobTaskInfos;
 import org.bukkit.ChatColor;
 
-public record Job(String name, ChatColor color,
-                  JobTaskInfos jobTaskInfos) {
-
-    public String getName() {
-        return name;
-    }
-
-    public ChatColor getColor() {
-        return color;
-    }
+public record Job(String getName, ChatColor getColor,
+                  JobTaskInfos getJobTaskInfos) {
 
     public String getColorizedName() {
-        return color + name;
-    }
-
-    public JobTaskInfos getJobTaskInfos() {
-        return jobTaskInfos;
+        return getColor + getName;
     }
 }
